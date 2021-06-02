705007b406edd901371717d8d050232457187ff4
PaveenKumarV <v.paveenkumarcse@gmail.com>
2021-06-03 00:30:55 +0530

    updates

--- functionsGo.go
+++ functionsGo.go
@@ -30,5 +30,5 @@ func main(){
 	fmt.Println("The Sum Of Given Two Numbers Are: ", result);
 	fmt.Println("The calculation Of Given Two Numbers Are: ", calcResult1 , calcResult2);
 	fmt.Println("The Multiple, Divide Of Given Two Numbers Are: ", calcMD1 , calcMD2);
-
+	fmt.Println("Bye");
 }
