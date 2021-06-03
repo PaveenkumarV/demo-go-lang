dbace4b6e8a7ecd6a8ab821521daf159e340698d
PaveenKumarV <v.paveenkumarcse@gmail.com>
2021-06-03 14:50:33 +0530

    Updated with Complex Var

--- variableGo.go
+++ variableGo.go
@@ -1,16 +1,22 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 
 	//  num1 := 10  // short hand declaration.
-	// // var num2 = 20  
+	// // var num2 = 20
 	// var num2 int
 	// num2 = 20
-	
-	var num1 , num2 int
+
+	var num1, num2 int
 
 	num1 = 20
 	num2 = 20
 
-	fmt.Println(num1 + num2);
+	var num3 complex128 = 1 + 2i
+	fmt.Printf("%v, %T\n", real(num3), real(num3))
+	fmt.Printf("%v, %T\n", imag(num3), imag(num3))
+
+	fmt.Println(num1 + num2)
 }
