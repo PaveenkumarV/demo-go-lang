084513a9bb20742a9eb1a8f0cab9f6a08a98e34c
PaveenKumarV <v.paveenkumarcse@gmail.com>
2021-06-06 20:36:40 +0530

    updated

--- packagemain.go
+++ packagemain.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt" // formatating text..
 
+const (
+	bmw = 1 << iota
+	audi
+	benz
+	landrover
+	toyota
+)
+
 func main() {
-	fmt.Println("Hello, World")
+	var num int64 = bmw | landrover | toyota
+	fmt.Printf("%b\n", num)
+	if (num & landrover) == landrover {
+		fmt.Println(true)
+	} else {
+		fmt.Println(false)
+	}
 }
